b6d5b2a972cb4b9cd2cf3f335a31f86231a928cb
Anderson Fabio <andersonfsjr@gmail.com>
2024-06-27 11:42:39 -0300

    Configuração H2

--- src/main/java/br/com/tokio/teste/transferenciasapi/TransferenciasApiApplication.java
+++ src/main/java/br/com/tokio/teste/transferenciasapi/TransferenciasApiApplication.java
@@ -5,14 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransferenciasApiApplication implements CommandLineRunner {
+public class TransferenciasApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransferenciasApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-	System.out.println("Hello World!");
 	}
 }
