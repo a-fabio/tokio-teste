59ab58b33decd2f919ad830743f4fcd6aeba2719
Anderson Fabio <andersonfsjr@gmail.com>
2024-06-28 05:25:49 -0300

    Persistência de taxas predefinidas na subida da aplicação & Endpoint para listagem de taxas. 2

--- src/main/java/br/com/tokio/teste/transferencias/service/TaxaService.java
+++ src/main/java/br/com/tokio/teste/transferencias/service/TaxaService.java
@@ -1,2 +1,13 @@
-package br.com.tokio.teste.transferencias.service;public interface TaxaService {
+package br.com.tokio.teste.transferencias.service;
+
+import br.com.tokio.teste.transferencias.model.Taxa;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface TaxaService {
+
+    List<Taxa> listar();
+
 }
